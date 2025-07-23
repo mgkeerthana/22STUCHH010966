a99320f5f6d5d2b01ea17c6ecc6aeb2f3505cc5b
M G Keerthana <mgkirthana@gmail.com>
2025-07-23 20:49:18 +0530

    Delete urlRouter.js

--- urlRouter.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-import express from 'express';
-import { nanoid } from 'nanoid';
-import { urlController } from '../controllers/urlController.js';
-
-app.post('/shorten', urlController.createShortUrl);
-app.post('/:shortUrl', urlController.retriveShortUrl);
